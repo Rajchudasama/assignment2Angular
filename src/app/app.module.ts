345233b78bdaf24e653eed8ef4da41f1f76002ae
raminder <raminderkaurmaan@yahoo.com>
2018-06-16 00:54:29 -0400

    Faq Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TestimonialsService } from './testimonials.service';
+import { FaqService } from './faq.service';
 
 import { AppComponent } from './app.component';
 import { DisplayTestimonialsComponent } from './display-testimonials/display-testimonials.component';
@@ -15,7 +16,9 @@ import { FaqComponent } from './faq/faq.component';
   imports: [
     BrowserModule
   ],
-  providers: [TestimonialsService],
+  providers: [TestimonialsService,
+  FaqService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
